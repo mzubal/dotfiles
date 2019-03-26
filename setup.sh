0753f2e7101d52df13e154804beac1b221b3d2b1
Miloš Zubal <milos.zubal@gmail.com>
2019-03-26 22:02:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ sudo pacman -Sy terminator
 sudo pacman -Sy zsh
 sudo pacman -Sy zsh-completions
 chsh -s /usr/bin/zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 xfconf-query -c xfce4-panel -p /panels/panel-0/position -n -t string -s "p=6;x=512;y=14"
