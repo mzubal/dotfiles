a776e95cd49bf5627bcc047378a80f80e4927a18
Miloš Zubal <milos.zubal@gmail.com>
2019-03-28 22:11:35 +0000

    Update and rename setup.sh to setup-zsh.sh

---
+++ setup-zsh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# zsh + oh-my-zsh
+sudo pacman -Sy zsh
+sudo pacman -Sy zsh-completions
+chsh -s /usr/bin/zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/.zshrc > ~/.zshrc
+source ~/.zshrc
+exit
