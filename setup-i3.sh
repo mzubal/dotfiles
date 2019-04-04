bc89f29ad5ca05be841a6e3480a3a0e662e91dd7
Miloš Zubal <milos.zubal@gmail.com>
2019-04-04 11:33:14 +0100

    Update setup-i3.sh

--- setup-i3.sh
+++ setup-i3.sh
@@ -1,13 +1,13 @@
 # Chromium
-pacin freetype2
-pacin chromium
+pacin freetype2 --noconfirm
+pacin chromium --noconfirm
 
 # gvim to integrate clipboard with vim, the vimrc to enable clipboard and map ctrl-c to copy from vim
-pacin gvim
+pacin gvim --noconfirm
 sudo sh -c 'wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/vimrc > /etc/vimrc'
 
 # atltab
-pacin uthash
+pacin uthash --noconfirm
 cd
 git clone https://github.com/sagb/alttab.git 
 cd alttab
@@ -15,20 +15,20 @@ cd alttab
 ./configure && sudo make install
 cd
 
-pacin tmux
-pacin rofi
+pacin tmux --noconfirm
+pacin rofi --noconfirm
 
 # i3 config
-sudo sh -c 'wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/i3.config > ~/.i3/config'
+sh -c 'wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/i3.config > ~/.i3/config'
 
 # CLI tools
-pacin thefuck
-pacin httpie
+pacin thefuck --noconfirm
+pacin httpie --noconfirm
 
 # Ansible
-pacin ansible
+pacin ansible --noconfirm
 
 # SDKMan
-pacin unzip
-pacin zip
+pacin unzip --noconfirm
+pacin zip --noconfirm
 curl -s "https://get.sdkman.io" | bash
