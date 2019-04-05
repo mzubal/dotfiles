26db2cbc93b3ec7a986c1dfad14742f176f61f98
Miloš Zubal <milos.zubal@gmail.com>
2019-04-05 15:00:01 +0100

    Update setup-zsh.sh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,7 +4,5 @@
 sudo pacman -Sy zsh
 sudo pacman -Sy zsh-completions
 chsh -s /usr/bin/zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" || true
 wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/.zshrc > ~/.zshrc
-source ~/.zshrc
-exit
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" || true
