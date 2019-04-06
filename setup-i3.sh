01447dd9d26372b34e4111ac2dd3d29800d232b7
Miloš Zubal <milos.zubal@gmail.com>
2019-04-06 19:27:47 +0000

    Update setup-i3.sh

--- setup-i3.sh
+++ setup-i3.sh
@@ -2,6 +2,9 @@
 pacin freetype2 --noconfirm
 pacin chromium --noconfirm
 
+# Qutebrowser
+pacin qutebrowser --noconfirm
+
 # gvim to integrate clipboard with vim, the vimrc to enable clipboard and map ctrl-c to copy from vim
 pacin gvim --noconfirm
 sudo sh -c 'wget -qO - https://raw.githubusercontent.com/mzubal/dotfiles/master/vimrc > /etc/vimrc'
